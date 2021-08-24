14fc666a17e7ed65aa5e36144ed1cbd0a31dccbc
alittriutari <alit.appkey@gmail.com>
2021-08-24 09:20:40 +0800

    initial

---
+++ android/sdk/build/generated/source/buildConfig/release/org/jitsi/meet/sdk/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.jitsi.meet.sdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "org.jitsi.meet.sdk";
+  public static final String BUILD_TYPE = "release";
+  // Field from build type: release
+  public static final boolean GOOGLE_SERVICES_ENABLED = false;
+  // Field from build type: release
+  public static final boolean LIBRE_BUILD = false;
+}
